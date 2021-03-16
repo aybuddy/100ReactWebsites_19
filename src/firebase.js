e4008dd3eb91407c19e13637e557dfd1665d6a89
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 18:49:09 -0400

    Clean up files, set up firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfnH9n5itCqO9OXKDMlRbkA3I7UNkbU6E',
+  authDomain: 'whatsapp-clone-cp-859d9.firebaseapp.com',
+  projectId: 'whatsapp-clone-cp-859d9',
+  storageBucket: 'whatsapp-clone-cp-859d9.appspot.com',
+  messagingSenderId: '553047364848',
+  appId: '1:553047364848:web:ddf9e9760f023f4cb91e35',
+};
