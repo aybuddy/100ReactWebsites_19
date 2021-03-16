9c6d4b3c75d566b594dc6272ebaad597e4f33dea
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 19:15:36 -0400

    Style the sidebar search container and input field

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import { Chat, DonutLarge, MoreVert } from '@material-ui/icons';
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
 import React from 'react';
 import './Sidebar.css';
 
@@ -20,8 +20,15 @@ const Sidebar = () => {
           </IconButton>
         </div>
       </div>
-      <div className='sidebar__search'></div>
-      <div className='sidebar__chats'></div>
+      <div className='sidebar__search'>
+        <div className='sidebar__searchContainer'>
+          <SearchOutlined />
+          <input placeholder='Search or start new chat' type='text' />
+        </div>
+      </div>
+      <div className='sidebar__chats'>
+        <SidebarChat />
+      </div>
     </div>
   );
 };
