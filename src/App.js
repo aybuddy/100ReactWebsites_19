d170b0e1adb3a5837cb6a73a196752d57c3871bc
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 19:06:21 -0400

    Install material ui, style headerRight and buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className='app'>
-      <h1>WhatsApp clone</h1>
+      <div className='app__body'>
+        <Sidebar />
+        {/* <Chat /> */}
+      </div>
     </div>
   );
 }
